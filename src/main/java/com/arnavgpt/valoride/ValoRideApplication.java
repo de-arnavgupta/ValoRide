677277c82ac2b6cbef5fce2fad36a04cade7b700
de-arnavgupta <arnavgpt28@outlook.com>
2026-01-24 01:18:25 +0530

    first commit

---
+++ src/main/java/com/arnavgpt/valoride/ValoRideApplication.java
@@ -0,0 +1,13 @@
+package com.arnavgpt.valoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValoRideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arnavgpt/valoride/ValoRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnavgpt.valoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValoRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
